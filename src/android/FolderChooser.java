b222b097dfb9f1a562431590fd6feedde45f5859
Dmytro Ivashchenko <yokodima@gmail.com>
2020-01-16 22:59:10 +0200

    dev

--- src/android/FolderChooser.java
+++ src/android/FolderChooser.java
@@ -50,9 +50,6 @@ public class FolderChooser extends CordovaPlugin {
     private List<String> m_subdirs = null;
     private ArrayAdapter<String> m_listAdapter = null;
 
-    private final static String TAG = "com.box.android.sample.FileListActivity";
-
-    private FileListAdapter adapter;
     private String currentFolderId = "0";
 
     private static final String ACTION_OPEN = "open";
@@ -128,55 +125,6 @@ public class FolderChooser extends CordovaPlugin {
         }
 
         return false;
-    }
-
-    public void open(String path){
-        this.m_chosenDir = path;
-        boolean m_newFolderEnabled = true;
-                // Create DirectoryChooserDialog and register a callback 
-                DirectoryChooserDialog directoryChooserDialog = 
-                new DirectoryChooserDialog(this.cordova.getActivity(), 
-                    new DirectoryChooserDialog.ChosenDirectoryListener() 
-                {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
-                    @Override
-                    public void onChosenDir(String chosenDir) 
-                    {
-                        PluginResult result = new PluginResult(PluginResult.Status.OK, chosenDir);
-                        FolderChooser.this.setM_chosenDir(chosenDir);
-                        Toast.makeText(
-                        FolderChooser.this.cordova.getActivity().getApplicationContext(), "Chosen directory: " + 
-                          chosenDir, Toast.LENGTH_LONG).show();
-                        result.setKeepCallback(false);
-                        if (FolderChooser.this.getC() != null) {
-                            FolderChooser.this.getC().sendPluginResult(result);
-                            FolderChooser.this.setC(null);
-                        }
-                    }
-                }); 
-                // Toggle new folder button enabling
-                directoryChooserDialog.setNewFolderEnabled(m_newFolderEnabled);
-                // Load directory chooser dialog for initial 'm_chosenDir' directory.
-                // The registered callback will be called upon final directory selection.                               
-                directoryChooserDialog.chooseDirectory(m_chosenDir);
-                m_newFolderEnabled = ! m_newFolderEnabled;
-    }
-    public void setM_chosenDir(String p){
-        this.m_chosenDir = p;
-    }
-    public String getM_chosenDir(){
-        return this.m_chosenDir;
-    }
-     public void setChosenDir(String p){
-        this.chosenDir = p;
-    }
-    public String getChosenDir(){
-        return this.chosenDir;
-    }
-    public CallbackContext getC(){
-        return this.c;
-    }
-    public void setC(CallbackContext c){
-        this.c = c;
     }
 
 
