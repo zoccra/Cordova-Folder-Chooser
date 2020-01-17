b533dbc285ade4ef82849c71667d820cf0ac36b3
Dmytro Ivashchenko <yokodima@gmail.com>
2020-01-17 17:16:11 +0200

    dev

--- src/android/FolderChooser.java
+++ src/android/FolderChooser.java
@@ -134,7 +134,7 @@ public class FolderChooser extends CordovaPlugin {
 //                        result.put("data", base64);
 //                        result.put("mediaType", mediaType);
 //                        result.put("name", name);
-                        result.put("uri", uri.getPath());
+                        result.put("uri", uri);
 
                         this.callback.success(result);
                     }
