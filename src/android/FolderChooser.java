9555228a86f42025e1f92c2e908be22d0e7cd016
Dmytro Ivashchenko <yokodima@gmail.com>
2020-01-20 15:32:02 +0200

    dev

--- src/android/FolderChooser.java
+++ src/android/FolderChooser.java
@@ -140,7 +140,8 @@ public class FolderChooser extends CordovaPlugin {
         try {
             if (requestCode == FolderChooser.PICK_FOLDER_REQUEST && this.callback != null) {
                 if (resultCode == Activity.RESULT_OK) {
-                    Uri uri = data.getData();
+                    String uri = '';
+//                    Uri uri = data.getData();
 
                     Context context = this.cordova.getActivity().getApplicationContext();
 
