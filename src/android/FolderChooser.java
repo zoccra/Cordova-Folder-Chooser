8a73791fd114a902a7c548ae35c78e17af3519e5
Dmytro Ivashchenko <yokodima@gmail.com>
2020-01-23 11:35:29 +0200

    dev

--- src/android/FolderChooser.java
+++ src/android/FolderChooser.java
@@ -1,5 +1,16 @@
 package com.example.cordova.folderChooser;
 
+
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
 import android.app.Activity;
 import android.os.Environment;
 
@@ -22,28 +33,132 @@ public class FolderChooser extends CordovaPlugin {
 
     private CallbackContext callback;
 
-    private void chooseFile (CallbackContext callbackContext, String accept) {
-        try {
-            Context context = this.cordova.getActivity().getApplicationContext();
-            String uri = "";
+    private static final String ACTION_OPEN = "open";
+    private static final int PICK_FOLDER_REQUEST = 1;
+    private static final String TAG = "Chooser";
 
-            for (File f : context.getExternalFilesDirs("")) {
-                if (Environment.isExternalStorageRemovable(f)) {
-                    uri = f.getAbsolutePath();
-                    try {
-                        new File(f.getAbsolutePath() + "/testFile.txt").createNewFile();
-                    } catch (IOException err) {
-                        this.callback.error("Execute create file is failed: " + err.toString());
+    /** @see https://stackoverflow.com/a/17861016/459881 */
+    public static byte[] getBytesFromInputStream (InputStream is) throws IOException {
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        byte[] buffer = new byte[0xFFFF];
+
+        for (int len = is.read(buffer); len != -1; len = is.read(buffer)) {
+            os.write(buffer, 0, len);
+        }
+
+        return os.toByteArray();
+    }
+
+    /** @see https://stackoverflow.com/a/23270545/459881 */
+    public static String getDisplayName (ContentResolver contentResolver, Uri uri) {
+        String[] projection = {MediaStore.MediaColumns.DISPLAY_NAME};
+        Cursor metaCursor = contentResolver.query(uri, projection, null, null, null);
+
+        if (metaCursor != null) {
+            try {
+                if (metaCursor.moveToFirst()) {
+                    return metaCursor.getString(0);
+                }
+            } finally {
+                metaCursor.close();
+            }
+        }
+
+        return "File";
+    }
+
+    private void chooseFile (CallbackContext callbackContext, String accept) {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+//        intent.setType("*/*");
+//        if (!accept.equals("*/*")) {
+//            intent.putExtra(Intent.EXTRA_MIME_TYPES, accept.split(","));
+//        }
+//        intent.addCategory(Intent.CATEGORY_OPENABLE);
+//        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false);
+//        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
+
+        Intent chooser = Intent.createChooser(intent, "Open folder");
+        cordova.startActivityForResult(this, chooser, FolderChooser.PICK_FOLDER_REQUEST);
+
+        PluginResult pluginResult = new PluginResult(PluginResult.Status.NO_RESULT);
+        pluginResult.setKeepCallback(true);
+        this.callback = callbackContext;
+        callbackContext.sendPluginResult(pluginResult);
+//
+//        try {
+//            Context context = this.cordova.getActivity().getApplicationContext();
+//            String uri = "";
+//
+//            for (File f : context.getExternalFilesDirs("")) {
+//                if (Environment.isExternalStorageRemovable(f)) {
+//                    uri = f.getAbsolutePath();
+//                    try {
+//                        new File(f.getAbsolutePath() + "/testFile.txt").createNewFile();
+//                    } catch (IOException err) {
+//                        this.callback.error("Execute create file is failed: " + err.toString());
+//                    }
+//                }
+//            }
+//
+//            JSONObject result = new JSONObject();
+//            result.put("uri", uri);
+//            callbackContext.success(result);
+//
+//        } catch (JSONException err) {
+//            this.callback.error("Execute failed: " + err.toString());
+//        }
+    }
+
+    @Override
+    public void onActivityResult (int requestCode, int resultCode, Intent data) {
+        try {
+            if (requestCode == FolderChooser.PICK_FOLDER_REQUEST && this.callback != null) {
+                if (resultCode == Activity.RESULT_OK) {
+                    Uri uri = data.getData();
+                    String sourceFilename= uri.getPath();
+                    String destinationFilename = android.os.Environment.getExternalStorageDirectory().getPath()+File.separatorChar + "abc.mp3";
+
+                    if (uri != null) {
+//                        ContentResolver contentResolver =
+//                                this.cordova.getActivity().getContentResolver()
+//                                ;
+//
+//                        String name = FolderChooser.getDisplayName(contentResolver, uri);
+//
+//                        String mediaType = contentResolver.getType(uri);
+//                        if (mediaType == null || mediaType.isEmpty()) {
+//                            mediaType = "application/octet-stream";
+//                        }
+//
+//                        byte[] bytes = FolderChooser.getBytesFromInputStream(
+//                                contentResolver.openInputStream(uri)
+//                        );
+//
+//                        String base64 = Base64.encodeToString(bytes, Base64.DEFAULT);
+//
+                        JSONObject result = new JSONObject();
+
+//                        result.put("data", base64);
+//                        result.put("mediaType", mediaType);
+//                        result.put("name", name);
+                        result.put("uri", destinationFilename);
+
+                        this.callback.success(result);
+                    }
+                    else {
+                        this.callback.error("Folder URI was null.");
                     }
                 }
+                else if (resultCode == Activity.RESULT_CANCELED) {
+                    this.callback.success("RESULT_CANCELED");
+                }
+                else {
+                    this.callback.error(resultCode);
+                }
             }
-
-            JSONObject result = new JSONObject();
-            result.put("uri", uri);
-            callbackContext.success(result);
-
-        } catch (JSONException err) {
-            this.callback.error("Execute failed: " + err.toString());
+        }
+        catch (Exception err) {
+            this.callback.error("Failed to read folder: " + err.toString());
         }
     }
 
