6d00e398da7fd4b53a51731e6558eeac27c475b9
Dmytro Ivashchenko <yokodima@gmail.com>
2020-01-23 17:04:53 +0200

    dev

--- www/plugin.js
+++ www/plugin.js
@@ -3,3 +3,7 @@ var exec = require('cordova/exec');
 exports.saveFileToUSB = function (path, onSuccess, onFail) {
     exec(onSuccess, onFail, "FolderChooser", "saveFileToUSB", [path]);
 };
+
+exports.getBackupsFromUSB = function (uri, onSuccess, onFail) {
+    exec(onSuccess, onFail, "FolderChooser", "getBackupsFromUSB", [uri]);
+};
