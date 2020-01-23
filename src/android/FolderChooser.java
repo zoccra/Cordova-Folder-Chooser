1097b23d4532a757c9d25d24fc71df3cc0b540fc
Dmytro Ivashchenko <yokodima@gmail.com>
2020-01-23 12:28:09 +0200

    dev

--- src/android/FolderChooser.java
+++ src/android/FolderChooser.java
@@ -26,6 +26,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.content.Context;
+
 import java.util.ArrayList;
 
 public class FolderChooser extends CordovaPlugin {
@@ -35,10 +36,13 @@ public class FolderChooser extends CordovaPlugin {
 
     private static final String ACTION_OPEN = "open";
     private static final int PICK_FOLDER_REQUEST = 1;
+    private static final int CREATE_REQUEST_CODE = 2;
     private static final String TAG = "Chooser";
 
-    /** @see https://stackoverflow.com/a/17861016/459881 */
-    public static byte[] getBytesFromInputStream (InputStream is) throws IOException {
+    /**
+     * @see https://stackoverflow.com/a/17861016/459881
+     */
+    public static byte[] getBytesFromInputStream(InputStream is) throws IOException {
         ByteArrayOutputStream os = new ByteArrayOutputStream();
         byte[] buffer = new byte[0xFFFF];
 
@@ -49,8 +53,10 @@ public class FolderChooser extends CordovaPlugin {
         return os.toByteArray();
     }
 
-    /** @see https://stackoverflow.com/a/23270545/459881 */
-    public static String getDisplayName (ContentResolver contentResolver, Uri uri) {
+    /**
+     * @see https://stackoverflow.com/a/23270545/459881
+     */
+    public static String getDisplayName(ContentResolver contentResolver, Uri uri) {
         String[] projection = {MediaStore.MediaColumns.DISPLAY_NAME};
         Cursor metaCursor = contentResolver.query(uri, projection, null, null, null);
 
@@ -67,13 +73,13 @@ public class FolderChooser extends CordovaPlugin {
         return "File";
     }
 
-    private void chooseFile (CallbackContext callbackContext, String accept) {
-        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
-//        intent.setType("*/*");
+    private void chooseFile(CallbackContext callbackContext, String accept) {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.setType("application/vnd.android.package-archive");
 //        if (!accept.equals("*/*")) {
 //            intent.putExtra(Intent.EXTRA_MIME_TYPES, accept.split(","));
 //        }
-//        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
 //        intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, false);
 //        intent.putExtra(Intent.EXTRA_LOCAL_ONLY, true);
 
@@ -110,15 +116,95 @@ public class FolderChooser extends CordovaPlugin {
     }
 
     @Override
-    public void onActivityResult (int requestCode, int resultCode, Intent data) {
-        try {
-            if (requestCode == FolderChooser.PICK_FOLDER_REQUEST && this.callback != null) {
-                if (resultCode == Activity.RESULT_OK) {
-                    Uri uri = data.getData();
-                    String sourceFilename= uri.getPath();
-                    String destinationFilename = android.os.Environment.getExternalStorageDirectory().getPath()+File.separatorChar + "abc.mp3";
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == FolderChooser.PICK_FOLDER_REQUEST && this.callback != null) {
+            if (resultCode == Activity.RESULT_OK) {
+                Uri uri = data.getData();
 
-                    if (uri != null) {
+                final int takeFlags = data.getFlags() & (Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+                this.cordova.getActivity().getContentResolver().takePersistableUriPermission(uri, takeFlags);
+
+                getMetaData(uri);
+            } else {
+                this.callback.error("Folder URI was null.");
+            }
+        } else if (requestCode == FolderChooser.CREATE_REQUEST_CODE && this.callback != null) {
+            if (resultCode == Activity.RESULT_OK) {
+                Uri uri = data.getData();
+
+                getMetaData(uri);
+            } else {
+                this.callback.error("File create error.");
+            }
+        } else if (resultCode == Activity.RESULT_CANCELED) {
+            this.callback.success("RESULT_CANCELED");
+        } else {
+            this.callback.error(resultCode);
+        }
+    }
+
+    private Bitmap getBitmapFromUri(Uri uri) throws IOException {
+        ParcelFileDescriptor parcelFileDescriptor = this.cordova.getActivity().getContentResolver()
+                .openFileDescriptor(uri, "r");
+        FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+        Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+
+        parcelFileDescriptor.close();
+
+        return image;
+    }
+
+    private String reasTextFromUri(Uri uri) throws IOException {
+        InputStream inputStream = this.cordova.getActivity().getContentResolver().openInputStream(uri);
+        BufferedReader reader = new BufferReader(new InputStreamReader(inputStream));
+        StringBuilder stringBuilder = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            stringBuilder.append(line);
+        }
+        fileInputStream.close();
+        parcelFileDescriptor.close();
+        return stringBuilder.toString();
+    }
+
+    private String writeTextToUri(Uri uri) throws IOException {
+        OutputStream outputStream = this.cordova.getActivity().getContentResolver().openOutputStream(uri);
+        outputStream.write(("Text").getBytes());
+        outputStream.close();
+    }
+
+    private void createFile(String mimeType, String fileName) {
+        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+        intent.setType("application/vnd.android.package-archive");
+        intent.putExtra(Intent.EXTRA_TITLE, fileName);
+        startActivityForResult(intent, CREATE_REQUEST_CODE);
+    }
+
+    private void deleteFile(uri) {
+        DocumentsContract.deleteDocument(this.cordova.getActivity().getContentResolver(), uri);
+    }
+
+    private void getMetaData(uri) {
+        Cursor cursor = this.cordova.getActivity().getContentResolver()
+                .query(uri, null, null, null, null, null);
+
+        if (cursor != null && cursor.moveToFirst()) {
+            String displayName = cursor.getString(cursor.getColumnIndex(OpenableColumns.DISPLAY_NAME));
+
+            int sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE);
+            String size = null;
+            if (!cursor,isNull(sizeIndex)){
+                size = cursor.getString(sizeIndex);
+            } else{
+                size = "Unknown";
+            }
+        }
+        cursor.close();
+
+        String sourceFilename = uri.getPath();
+        String destinationFilename = android.os.Environment.getExternalStorageDirectory().getPath() + File.separatorChar + "abc.mp3";
+
+        if (uri != null) {
 //                        ContentResolver contentResolver =
 //                                this.cordova.getActivity().getContentResolver()
 //                                ;
@@ -136,34 +222,19 @@ public class FolderChooser extends CordovaPlugin {
 //
 //                        String base64 = Base64.encodeToString(bytes, Base64.DEFAULT);
 //
-                        JSONObject result = new JSONObject();
+            JSONObject result = new JSONObject();
 
 //                        result.put("data", base64);
 //                        result.put("mediaType", mediaType);
 //                        result.put("name", name);
-                        result.put("uri", destinationFilename);
+            result.put("uri", destinationFilename);
 
-                        this.callback.success(result);
-                    }
-                    else {
-                        this.callback.error("Folder URI was null.");
-                    }
-                }
-                else if (resultCode == Activity.RESULT_CANCELED) {
-                    this.callback.success("RESULT_CANCELED");
-                }
-                else {
-                    this.callback.error(resultCode);
-                }
-            }
-        }
-        catch (Exception err) {
-            this.callback.error("Failed to read folder: " + err.toString());
+            this.callback.success(result);
         }
     }
 
     @Override
-    public boolean execute (
+    public boolean execute(
             String action,
             JSONArray args,
             CallbackContext callbackContext
@@ -173,8 +244,7 @@ public class FolderChooser extends CordovaPlugin {
                 this.chooseFile(callbackContext, args.getString(0));
                 return true;
             }
-        }
-        catch (JSONException err) {
+        } catch (JSONException err) {
             this.callback.error("Execute failed: " + err.toString());
         }
 
